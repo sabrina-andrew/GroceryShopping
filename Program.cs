7bdb79b98e8f0a0aef740cf1d1e54a6890bf31a0
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-30 14:25:39 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GroceryShopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string guacomoleResponse;
+            bool isQuacomoleOnSale;
+            double  moneyInPocket;
+
+            Console.WriteLine("Is guacomole on sale?  Enter yes or no.");
+            guacomoleResponse = Console.ReadLine();
+
+           if (guacomoleResponse == "yes")
+            {    
+                isQuacomoleOnSale = true;
+            }
+            else
+            {
+                isQuacomoleOnSale = false;
+            }
+
+            Console.WriteLine("How much money is in your pocket? ");
+            moneyInPocket = Convert.ToDouble(Console.ReadLine());
+
+            if ((isQuacomoleOnSale && moneyInPocket >= 10) || moneyInPocket >= 100)
+            {
+                Console.WriteLine("Lets go shopping!");
+            }
+            else
+            {
+                Console.WriteLine("We better stay home. We don't have enough money.");
+            }
+
+        }
+    }
+}
